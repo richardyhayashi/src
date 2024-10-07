0643b1cc9b0713079f80db8aac1cad06e5eb8e04
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-10-07 23:18:05 +0900

    Finished Chapter #8.

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -6,10 +6,13 @@ import { useMediaQuery } from 'usehooks-ts';
 import { cn } from '@/lib/utils';
 import { usePathname } from 'next/navigation';
 import UserItem from './UserItem';
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 
 const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
+  const documents = useQuery(api.documents.get);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<'aside'>>(null);
@@ -118,7 +121,11 @@ const Navigation = () => {
           <UserItem />
         </div>
         <div className="mt-4">
-          <p>Document</p>
+          {documents?.map((document) => (
+            <p key={document._id}>
+              {document.title}
+            </p>
+          ))}
         </div>
         <div
           onMouseDown={handleMouseDown}
